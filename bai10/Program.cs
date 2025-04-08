066828741252badf873c24bd2423048d33d0550c
LeDuyHoa <152716817+hoaphun2@users.noreply.github.com>
2025-04-08 21:32:29 +0700

    Add files via upload

---
+++ bai10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Nhập số nguyên từ bàn phím
+        Console.Write("Nhap mot so nguyen: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Kiểm tra số nguyên tố
+        if (n < 2)
+        {
+            Console.WriteLine($"{n} khong phai la so nguyen to.");
+        }
+        else
+        {
+            bool laSoNguyenTo = true;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    laSoNguyenTo = false;
+                    break;
+                }
+            }
+
+            if (laSoNguyenTo)
+            {
+                Console.WriteLine($"{n} la so nguyen to.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} khong phai la so nguyen to.");
+            }
+        }
+
+        // Giữ màn hình console
+        Console.ReadKey();
+    }
+}
